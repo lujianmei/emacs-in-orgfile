c544b675cce0304556b8ca0b2b6d0ba54a8faea3
lujianmei <anysky130@163.com>
2016-07-23 22:28:21 +0800

    changed

--- init.el
+++ init.el
@@ -27,3 +27,23 @@
     (org-babel-load-file (expand-file-name "starter-kit.org" starter-kit-dir))))
 
 ;;; init.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ace-isearch-function (quote ace-jump-word-mode))
+ '(ace-isearch-input-length 6)
+ '(ace-isearch-jump-delay 0.25)
+ '(ace-isearch-use-jump (quote printing-char))
+ '(custom-enabled-themes (quote (sanityinc-solarized-dark)))
+ '(custom-safe-themes
+   (quote
+    ("4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" default)))
+ '(org-export-table-data-tags (quote ("<tr class=\"CUSTOM_ID>" . "</t>"))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
